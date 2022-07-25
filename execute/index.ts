b5d9b2ffb2b41466a4b6b1ddbfb9a96bd9207f7e
Davide Bizzi <davide.bizzi@app-quality.com>
2022-07-25 10:40:58 +0200

    test: Add test execute

---
+++ execute/index.ts
@@ -0,0 +1,94 @@
+import { APIGatewayEvent } from "aws-lambda";
+
+const body = JSON.stringify({
+  questions: [
+    {
+      title: "Quale è il tuo nome?",
+      type: "text",
+      cufId: 1,
+    },
+    {
+      title: "Quale è la tua banca?",
+      type: "multiselect",
+      cufId: 2,
+      options: [
+        { id: 1, value: "Banca1" },
+        { id: 2, value: "Banca2" },
+      ],
+    },
+    {
+      title: "Quant figli hai?",
+      type: "select",
+      cufId: 3,
+      options: [
+        { id: 3, value: "0" },
+        { id: 4, value: "1000" },
+      ],
+    },
+  ],
+});
+const event: APIGatewayEvent = {
+  body: body,
+  headers: {},
+  multiValueHeaders: {},
+  httpMethod: "",
+  isBase64Encoded: false,
+  path: "",
+  pathParameters: {},
+  queryStringParameters: {},
+  multiValueQueryStringParameters: {},
+  stageVariables: {},
+  requestContext: {
+    accountId: "",
+    apiId: "",
+    authorizer: {},
+    connectedAt: 0,
+    connectionId: "",
+    domainName: "",
+    protocol: "",
+    httpMethod: "",
+    identity: {
+      accessKey: "",
+      accountId: "",
+      apiKey: "",
+      apiKeyId: "",
+      caller: "",
+      cognitoAuthenticationProvider: "",
+      cognitoAuthenticationType: "",
+      cognitoIdentityId: "",
+      cognitoIdentityPoolId: "",
+      sourceIp: "",
+      user: "",
+      userArn: "",
+      userAgent: "",
+      principalOrgId: "",
+      clientCert: {
+        clientCertPem: "",
+        serialNumber: "",
+        subjectDN: "",
+        issuerDN: "",
+        validity: {
+          notAfter: "",
+          notBefore: "",
+        },
+      },
+    },
+    path: "",
+    stage: "",
+    requestId: "",
+    requestTimeEpoch: 0,
+    resourceId: "",
+    resourcePath: "",
+  },
+  resource: "",
+};
+
+import { main } from "../lambda";
+require("dotenv").config();
+
+async function test() {
+  const res = await main(event);
+  console.log(res);
+}
+
+test();
